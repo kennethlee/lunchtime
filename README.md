55757b251e5cb01bb991462d180fe3a8012cb818
Kenneth Lee <kennyxlee@gmail.com>
2015-02-01 23:17:17 -0800

    Create Suggestions controller

---
+++ README.md
@@ -0,0 +1,5 @@
+# "What's for lunch?"
+
+A practice rails app I made so that my coworkers and I can suggest and vote on what to have for lunch.
+
+Inspired by our indecisiveness.
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'suggestions/new'
+
+  get 'suggestions/show'
+
+  get 'suggestions/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
