4dccbcabb9b3a828e36b9c80cbd27dc7c70fb3df
Kenneth Lee <kennyxlee@gmail.com>
2015-02-02 21:34:16 -0800

    Force users to first login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # Authenticate user before showing any views
+  before_action :authenticate_user!
 end
