ae421c66bfc881adc7ffb17129d48107d4ac953f
Kenneth Lee <kennyxlee@gmail.com>
2015-02-02 19:08:42 -0800

    Suggestion controller actions

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -1,10 +1,36 @@
 class SuggestionsController < ApplicationController
+
+  def index
+    @suggestions = Suggestion.all
+  end
+
   def new
+    @suggestion = Suggestion.new
+  end
+
+  # Saves new suggestion
+  def create
+    @suggestion = Suggestion.new(suggestion_params)
+    if @suggestion.save
+    redirect_to suggestions_path
+    else
+    render 'new'
+    end
   end
 
   def show
   end
 
-  def index
+  # Destroys a submitted suggestion
+  def destroy
+    @suggestion.destroy
+    flash[:success] = "The suggestion has been deleted."
+    redirect_to root_url
   end
+
+  private
+
+    def suggestion_params
+      params.require(:suggestion).permit(:name, :user_id, :user_name)
+    end
 end
